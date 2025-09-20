928fcfa2e82902177d54fae686a655c44f6e2cb6
manirajdeva <tmaniraj1111@gmail.com>
2025-09-20 20:17:05 +0530

    updated code

--- migrations/V1__create_test_table.sql
+++ migrations/V1__create_test_table.sql
@@ -2,7 +2,7 @@ use database sample123;
 use schema sample_sch;
 
 -- V1__create_test_table.sql (safe test)
-CREATE OR REPLACE TABLE IF NOT EXISTS TEST_TABLE_CI (
+CREATE OR REPLACE TABLE TEST_TABLE_CI (
   ID INT,
   CREATED_AT TIMESTAMP_LTZ DEFAULT CURRENT_TIMESTAMP
 );
